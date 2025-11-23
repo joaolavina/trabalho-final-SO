521a456919af2e1692043523cbe2cc95dd648eac
Cristina SJ <sjcristina9@gmail.com>
2025-11-23 10:38:34 -0300

    GpuReader v2

--- MonitorGpu/services/GpuReader.cs
+++ MonitorGpu/services/GpuReader.cs
@@ -8,17 +8,15 @@ namespace MonitorGpu.Services
         {
             float usage = 0.0f;
             var searcher = new ManagementObjectSearcher("root\\CIMV2",
-                "SELECT * FROM Win32_PerfFormattedData_GPUPerformanceCounters_GPUEngine"); // whats 2
+                "SELECT * FROM Win32_PerfFormattedData_GPUPerformanceCounters_GPUEngine"); // select no Windows Management Instrumentation
 
             foreach (ManagementObject obj in searcher.Get())
             {
-                string name = obj["Name"]?.ToString() ?? ""; // whats
-                if (name.Contains("engtype_3D")) // foca em engine 3D POR QUE ?
-                {
+                if (obj["UtilizationPercentage"] != null)
                     usage += Convert.ToSingle(obj["UtilizationPercentage"]);
-                }
             }
-            return usage; // 0-100%
+
+            return usage;
         }
     }
 }
